2e865f0e2c13f96eaee658185e9a654dd19d880a
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2019-11-08 21:27:08 +0200

    NVS Cloud Scanner (#8)

---
+++ src/archiveUtils.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/nexus/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+import * as path from 'path';
+import * as fs from 'fs';
+import Archiver = require('archiver');
+import config from './config.json';
+
+export class ArchiveUtils {
+
+  static async zipFiles(files: Array<string>): Promise<string> {
+    return new Promise((resolve, reject) => {
+
+      if (files === undefined || files.length === 0) {
+        reject(new Error('no files to zip'));
+        return;
+      }
+
+      const zipFile = path.join(__dirname, config.uploadService.zipFileName);
+      const zipOut = fs.createWriteStream(zipFile);
+      const archive = Archiver('zip', {
+        zlib: {level: 9}
+      });
+
+      archive.pipe(zipOut);
+      for (const file of files) {
+        archive.append(fs.createReadStream(file), {name: file});
+      }
+
+      archive.on('error', (err) => {
+        reject(err);
+        return;
+      });
+
+      zipOut.on('close', () => {
+        resolve(zipFile);
+        return;
+      });
+
+      archive.finalize();
+    });
+
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc. All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/nexus/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+import * as core from '@actions/core';
+import FileUploadService from './fileUploadService';
+import {findFiles} from './scanPattern';
+import {ArchiveUtils} from './archiveUtils';
+import * as EmailValidator from 'email-validator';
+import LoggingUtils from './loggingUtils';
+
+async function run() {
+  try {
+    const email = core.getInput('email');
+    if (!EmailValidator.validate(email)) {
+      core.setFailed(`${email} email is not valid`);
+    }
+    const password = core.getInput('password');
+    const directory = core.getInput('directory');
+
+    LoggingUtils.logSeparator();
+    LoggingUtils.logInputField('email', email);
+    LoggingUtils.logInputField('password', '******');
+    LoggingUtils.logInputField('directory', directory);
+    LoggingUtils.logSeparator();
+
+    const matchedFiles = await findFiles(directory);
+    LoggingUtils.logMessage(`found ${matchedFiles.length} files to scan`);
+    LoggingUtils.logMessage('archiving files...');
+    const archiveFilePath = await ArchiveUtils.zipFiles(matchedFiles);
+
+    const fileUploadService = FileUploadService.from(archiveFilePath, email, password);
+    try {
+      LoggingUtils.logMessage('upload zip file to NVS...');
+      const successUrl = await fileUploadService.uploadFile();
+      LoggingUtils.logMessage(`Success url: ${successUrl}`);
+    }
+    catch (error) {
+      core.setFailed(error);
+    }
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
